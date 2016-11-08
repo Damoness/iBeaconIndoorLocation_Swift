9644a963690464fa1150668ddf2bc86dc9fd28cc
TonyLin <qjwdcool@qq.com>
2016-11-08 21:22:02 +0800

    1.增加设备配置界面

---
+++ iBeaconIndoorLocation_Swift/DeployDeviceViewController.swift
@@ -0,0 +1,39 @@
+//
+//  DeployDeviceViewController.swift
+//  iBeaconIndoorLocation_Swift
+//
+//  Created by 吴狄 on 2016/11/8.
+//  Copyright © 2016年 ditiy. All rights reserved.
+//
+
+import UIKit
+
+class DeployDeviceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.navigationItem.title = "配置设备"
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
